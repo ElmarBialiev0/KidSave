fd1c80f27873ebc95a29891ee53cfa3ce6588575
archeryfox <89808784+archeryfox@users.noreply.github.com>
2024-03-04 10:18:43 +0300

    efefe

--- SaveKid/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import *
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", index_page, name='index_page'),
-    path("buy/", buy_page, name='buy_page'),
-    path("about/", about_page, name='about_page'),
-]
